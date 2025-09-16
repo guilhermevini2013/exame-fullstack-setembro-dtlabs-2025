5399ff18418f46fb59eb070eb3c11a13ed2ce67f
guilhermevini2013 <guilhermevini2013@hotmail.com>
2025-09-16 13:28:43 -0300

    initial commit

--- device-monitoring-api/src/__init__.py
+++ device-monitoring-api/src/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from .controllers.device import bp_device as device_bp
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "postgresql://admin:admin@localhost:5432/device_monitoring"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 def create_app():
     app = Flask(__name__)
     # routes
     app.register_blueprint(device_bp, url_prefix="/api")
+    Base.metadata.create_all(bind=engine)
 
     return app
