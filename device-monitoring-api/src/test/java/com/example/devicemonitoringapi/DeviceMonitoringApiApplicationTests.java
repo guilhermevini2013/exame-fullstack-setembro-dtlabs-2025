14c756e84ed882566ab0665e7c7bce46c9acd0d3
guilhermevini2013 <guilhermevini2013@hotmail.com>
2025-09-16 19:11:50 -0300

    feat: User Auth implemented

---
+++ device-monitoring-api/src/test/java/com/example/devicemonitoringapi/DeviceMonitoringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devicemonitoringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMonitoringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
